6c1a5eaea56edd71f9bf232ef86adb927513a1d1
Yun-Yunho <yoon36399@naver.com>
2021-10-28 16:30:49 +0900

    Setting: DB connect

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+const mysql  = require('mysql2');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const connnection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+const baseUrl = "https://kr.api.riotgames.com";
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (req, res, next) => {
+  const nickname = "어시가짜다원딜아";
+  const url = baseUrl + "/lol/summoner/v4/summoners/by-name/" +
+              encodeURI(nickname) + "?api_key=" + process.env.API_KEY;
+  const { data } = await axios.get(url);
+  console.log(data);
+  res.send(data);
 });
 
 module.exports = router;
