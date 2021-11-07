d8533cf38e98045ac2f9252a3df22fee997cb5d6
8471919 <7471919@naver.com>
2021-11-07 23:11:32 +0900

    fix:데이터베이스를 connect에서 pool로 변경 #0

--- src/database/db.js
+++ src/database/db.js
@@ -1,18 +1,17 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 module.exports = (function () {
-    const connection = mysql.createConnection({
+    const pool = mysql.createPool({
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        port: Number(process.env.DB_PORT),
         user: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
+        connectionLimit: 8,
     });
 
-    connection.connect();
-
-    return connection;
+    return pool;
 })();
