b2b97b7a46d700a621e4381075cacf3b1cc6f658
8471919 <7471919@naver.com>
2021-11-11 17:36:42 +0900

    style: main class의 소환사 정보와 matchId를 가져오는 함수 정리 #0

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -9,29 +9,27 @@ class Main {
         this.search.addEventListener("click", async () => {
             const nickname = document.getElementsByName("nickname")[0].value;
 
-            console.log(encodeURI(nickname));
-            const res = await fetch(`/summoners/info?nickname=${nickname}`, {
-                headers: { "Content-Type": "application/json" },
-                method: "GET",
-            });
-            console.log(res);
+            //Summoner의 NAME, PUUID 등 정보를 가져옴
+            const resultOfSummonerInfo = await fetch(
+                `/summoners/info?nickname=${nickname}`,
+                {
+                    headers: { "Content-Type": "application/json" },
+                    method: "GET",
+                }
+            );
 
-            const res2 = await fetch(`/summoners/match?nickname=${nickname}`, {
-                headers: { "Content-Type": "application/json" },
-                method: "GET",
-            });
-            console.log(res2);
+            const jsonOfSummonerInfo = await resultOfSummonerInfo.json();
 
-            const jsonRes = await res.json();
-            const jsonRes2 = await res2.json();
+            //Summoner nickname을 이용해서 최근 20게임의 matchId를 가져옴
+            const resultOfMatchId = await fetch(
+                `/summoners/match?nickname=${nickname}`,
+                {
+                    headers: { "Content-Type": "application/json" },
+                    method: "GET",
+                }
+            );
 
-            console.log(jsonRes);
-            console.log(jsonRes2);
-
-            const n = document.createElement("div");
-            n.innerHTML = jsonRes.name;
-            n.innerHTML = jsonRes2;
-            this.body.appendChild(n);
+            const jsonOfMatchId = await resultOfMatchId.json();
         });
     }
 }
