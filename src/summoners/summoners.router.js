c2089e560e5e1df6ff0e755291cbb0855f1ff951
8471919 <7471919@naver.com>
2021-11-15 13:55:20 +0900

    fix:getPage함수를 getMainPage로 변경 #0

--- src/summoners/summoners.router.js
+++ src/summoners/summoners.router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const SummonersController = require("./summoners.controller");
 
-router.get("/", SummonersController.getPage);
+router.get("/", SummonersController.getMainPage);
 router.get("/info", SummonersController.getSummoner);
 router.get("/match", SummonersController.getMatch);
 
